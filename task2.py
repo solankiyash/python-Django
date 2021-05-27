ef22ed09157eec67b25e32b29f42bf54bacfc2d1
solankiyash <84774663+solankiyash@users.noreply.github.com>
2021-05-27 13:16:13 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,62 @@
+#print Hello world
+print('Hello world')
+
+a=26
+b=40.5
+c="solanki yash"
+
+print(a)
+print("value of b is : ",b)
+print("value of c is : ", c)
+
+name="solanki yash"
+print=("Name is :",name)
+
+#assigning a new value to website
+name="kothari bhavy"
+print("Name is :",name) 
+
+#assigning the same value to multiple variables
+b = c = d = 25
+print("value of a is :",b)
+print("value of a is :",c)
+print("value of a is :",d)
+
+n1 = 20
+print (n1,"is of type",type(n1))
+n3 = 1 + 2j
+print(n3,"is complex number?",isinstance(1 + 2j, complex))
+
+nmae = "solanki yash"
+ # print complete string
+ print("Name is :",name)
+
+# prints chracters starting from 3rd to 5th
+print(name[2:5])
+
+# print chracters starting from 3rd character
+print(name[3:])
+
+# print string two times
+print(name * 2)
+
+# prints concatenated string
+print(name + "hello")
+
+list1 = [10,30,45,"yash",55,66,"solanki"]
+#list[2] = 30
+#list[0:4] = [10,30,45,yash]
+print("list1[2] = ", list[2])
+print("list1[0:4] =",list1[0:4])
+tuplel = (20,40,"yash",50)
+
+#tuplel[0:3] = [20,40,yash]
+print("tuplel[0:3]=",tuplel[0:3])
+
+d = { 2: 'yash', 3: 'vedang','key':20 }
+print("d[2] = ", d[1])
+print("d[3] = ", d[3]) 
+print("d['key'] =",d['key'] )
+
+
+
